b778dd74ba25c77f03286dfb65587e8b82488cd7
Viktor <korolev-viktor92@mail.ru>
2022-10-03 12:59:41 +0400

    Задача 38 готова

--- Program.cs
+++ Program.cs
@@ -9,16 +9,24 @@
 // Console.WriteLine($"Количесто четных элементов в заданном массиве = {even}");
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-
 // [3, 7, 23, 12] -> 19
-
 // [-4, -6, 89, 6] -> 0
 
-int[] array = GetArray(10, 10, 20);
-Console.WriteLine($"[{String.Join(", ", array)}]"!);
-int result = NoEvenNumSum(array);
-Console.WriteLine($"Сумма всех элементов стоящих на нечетных позициях в заданном массиве = {result}"!);
+// int[] array = GetArray(10, 10, 20);
+// Console.WriteLine($"[{String.Join(", ", array)}]"!);
+// int result = NoEvenNumSum(array);
+// Console.WriteLine($"Сумма всех элементов стоящих на нечетных позициях в заданном массиве = {result}"!);
 
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] array = GetArray(10, 100, 999);
+Console.WriteLine($"[{String.Join(", ", array)}]"!);
+int max = FindMaxNum(array);
+int min = FindMinNum(array);
+int result = max - min;
+Console.WriteLine($"Разницу между максимальным и минимальным значеним элементов массива = {result},"!);
+Console.WriteLine($"максимальноезначение элемента = {max} минимальное значение элемента {min}."!);
 
 int[] GetArray(int size, int minValue, int maxValue)
 {
@@ -51,4 +59,26 @@ int NoEvenNumSum(int[] array)
       sum += array[i];
     }
     return sum;
+}
+
+int FindMaxNum(int[] array)
+{
+  max = int.MinValue;
+  for (int i = 0; i < array.Length; i++)
+  if (array[i] > max)
+  {
+    max = array[i];
+  }
+  return max;
+}
+
+int FindMinNum(int[] array)
+{
+  min = int.MaxValue;
+  for (int i = 0; i < array.Length; i++)
+  if (array[i] < min)
+  {
+    min = array[i];
+  }
+  return min;
 }
