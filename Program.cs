cb2b583e77b408f75f1f92e1a1d8deb95102e4e0
Viktor <korolev-viktor92@mail.ru>
2022-10-03 11:10:20 +0400

    задача 36 готова

--- Program.cs
+++ Program.cs
@@ -8,6 +8,17 @@
 // int even = EvenNum(array);
 // Console.WriteLine($"Количесто четных элементов в заданном массиве = {even}");
 
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] array = GetArray(10, 10, 20);
+Console.WriteLine($"[{String.Join(", ", array)}]"!);
+int result = NoEvenNumSum(array);
+Console.WriteLine($"Сумма всех элементов стоящих на нечетных позициях в заданном массиве = {result}"!);
+
 
 int[] GetArray(int size, int minValue, int maxValue)
 {
@@ -29,4 +40,15 @@ int EvenNum(int[] array)
       a++;
     }
     return a;
+}
+
+int NoEvenNumSum(int[] array)
+{
+  int sum = 0;
+  for (int i = 0; i < array.Length; i++)
+  if (i % 2 != 0)
+    {
+      sum += array[i];
+    }
+    return sum;
 }
